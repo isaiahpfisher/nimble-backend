c33ab176572b8733cea36a83c250a2e1854de52d
Placide <placidestana12@gmail.com>
2026-07-21 17:10:24 -0500

    sprints setup

--- app/routes/sprint.routes.js
+++ app/routes/sprint.routes.js
@@ -3,8 +3,25 @@ module.exports = (app) => {
   const { authenticateRoute } = require("../authentication/authentication");
   var router = require("express").Router();
 
-  // Retrieve all Sprint
-  router.get("/sprints/", Sprint.findAll);
+  // Create a new Sprint
+  router.post("/sprints/", [authenticateRoute], Sprint.create);
+
+  // Create a series of recurring Sprints
+  router.post(
+    "/sprints/recurring",[authenticateRoute],Sprint.createRecurring);
+
+  // Retrieve all Sprints for a project
+  router.get(
+    "/projects/:projectId/sprints",[authenticateRoute],Sprint.findAllForProject,);
+
+  // Retrieve a single Sprint with id
+  router.get("/sprints/:id", [authenticateRoute], Sprint.findOne);
+
+  // Update a Sprint with id
+  router.put("/sprints/:id", [authenticateRoute], Sprint.update);
+
+  // Delete a Sprint with id
+  router.delete("/sprints/:id", [authenticateRoute], Sprint.delete);
 
   app.use("/nimbleapi", router);
 };
